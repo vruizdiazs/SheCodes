c0b756b24d7da778897541dd967ea28adc4b1c05
vruizdiazs <143648655+vruizdiazs@users.noreply.github.com>
2025-07-04 20:26:07 -0300

    update 01
     apply now with struggles

---
+++ scw2less07.js
@@ -0,0 +1,10 @@
+<script>
+      let country = prompt("What country are you from?");
+        let continent = prompt("Which continent are you from?");
+
+        if (country === "brazil" && country === "portugal") {
+            alert ("You speak portuguese")  
+        } else {
+          alert("You are not from Portugal")
+        }
+</script>
